4ce38f1917749ed8a3136e2fe18e66646a659212
charithlanzzz <lansakaracharith@gmail.com>
2022-12-19 09:47:49 +0530

    changes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -40,8 +40,8 @@ class OrderController extends Controller
             })
            ->addColumn('action', function ($orders) {
                 return '<div >
-               <a href="' . url('/orders/' . $orders->id . '/edit') . '" class="btn btn" style="background-color:#09560D!important;color:white;margin-top:10px;" ><i class="fa fa-pencil-square" aria-hidden="true"></i></a>
-               <button class="btn btn btn_delete " data-id="' . $orders->id . '" style="background-color:#CF0808!important;color:white;width:45px;margin-top: 10px;"><i class="fa fa-trash" aria-hidden="true"></i></button>
+               <a href="' . url('/orders/' . $orders->id . '/edit') . '" class="btn btn-primary" style="!important;color:white;margin-top:10px;" ><i class="fa fa-eye" aria-hidden="true"></i></a>
+
 
 
            </div>  ';
